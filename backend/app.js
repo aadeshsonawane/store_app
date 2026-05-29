0888c80719f93117165472b8b59f7d91ac073536
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-05-29 22:46:01 +0530

    add auth

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const db = require('./models/db');
+const authRoutes = require('./routes/auth');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
