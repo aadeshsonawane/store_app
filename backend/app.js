4b87e15ae272904911d4f7a08ae8d2211551acd1
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-05-29 18:07:17 +0530

    connect db with project in db.js file

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
+require('dotenv').config()
+const db = require('./models/db');
 const app = express();
 
 app.use(cors());
