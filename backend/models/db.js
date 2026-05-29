8b75cc0bcf73b698949bdcf5490d283f2d93a18f
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-05-29 22:26:10 +0530

    add auth.js file

--- backend/models/db.js
+++ backend/models/db.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.error('Database connec failed:', err);
+    console.error('Database connect failed:', err);
     return;
   }
   console.log('mysql connected');
